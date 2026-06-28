60deb460b547ae340cbcca294c30476cba24e1f4
Ekwere Noble <ekweerenoble2@gmail.com>
2026-06-28 18:23:56 +0100

    phase 2.3 & 2.4

---
+++ backend/utils/security.py
@@ -0,0 +1,14 @@
+import bcrypt
+
+def hash_password(password: str ) -> str:
+    """Hashes password using bcrypt"""
+    password_byte = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash_password = bcrypt.hashpw(password_byte, salt)
+    return hash_password.decode('utf-8')
+def verify_password(password: str, hashed: str) -> bool:
+    """verifier for hashed password"""
+    password_byte = password.encode('utf-8')
+    hashed_byte = hashed.encode('utf-8')
+    status = bcrypt.checkpw(password_byte, hashed_byte)
+    return status
